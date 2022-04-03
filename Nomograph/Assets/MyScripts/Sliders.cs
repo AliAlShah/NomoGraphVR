b50326cf6b535110f2653980907948cbd4780dd8
AliAlShah <96441735+AliAlShah@users.noreply.github.com>
2022-04-03 20:39:55 +0300

    Added a control Panel
    We made a control panel with two sliders representing x and y so now the user can control the nomogram

---
+++ Nomograph/Assets/MyScripts/Sliders.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sliders : MonoBehaviour
+{
+
+    public GameObject xSlider;
+    public GameObject ySlider;
+
+    private Rigidbody xRB;
+    private Rigidbody yRB;
+
+    void Start()
+    {
+        xRB = xSlider.GetComponent<Rigidbody>();
+        yRB = ySlider.GetComponent<Rigidbody>();
+    }
+
+    public void freezeSliders()
+    {
+        xRB.velocity = Vector3.zero;
+        yRB.velocity = Vector3.zero;
+    }
+
+
+
+    void Update()
+    {
+        
+    }
+}
