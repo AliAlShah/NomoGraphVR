b4f682510611bf4473b1f28ca9bdca926cb2e015
AliAlShah <96441735+AliAlShah@users.noreply.github.com>
2022-04-04 12:52:29 +0300

    Added ray interactor and formula grapher
    We added a canvas for the user to select the coefficients of x and y in the formula ax + by + c = 0 so now the user can create lines by inputting three values

---
+++ Nomograph/Assets/MyScripts/UIManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject linePoint;
+    public GameObject coordinatePlane;
+    private GameObject obj;
+
+    public Slider aSlider;
+    public Slider bSlider;
+    public Slider cSlider;
+
+    public Text aTxt;
+    public Text bTxt;
+    public Text cTxt;
+
+    private float aVal;
+    private float cVal;
+    private float bVal;
+    private float xVal;
+    private float yVal;
+
+    private Vector2 coordinate;
+    private Vector3 spawnPoint;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        aTxt.text = aSlider.value.ToString();
+        bTxt.text = bSlider.value.ToString();
+        cTxt.text = cSlider.value.ToString();
+    }
+
+    public void DrawFunction()
+    {
+        aVal = aSlider.value / 10;
+        bVal = bSlider.value / 10;
+        cVal = cSlider.value / 10;
+        xVal = -3;
+
+        while(xVal < 3)
+        {
+            yVal = ((-aVal * xVal) - cVal) / bVal;
+            coordinate = new Vector2(xVal, yVal);
+            Debug.Log(coordinate);
+
+            spawnPoint = new Vector3(xVal, yVal, -2.5f);
+            obj = Instantiate(linePoint, spawnPoint, Quaternion.identity);
+            obj.transform.parent = coordinatePlane.transform;
+            xVal += 0.25f;
+        }
+    }
+}
